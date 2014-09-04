be12946c8643f4a3951cf85bb6a50dce7bf2d79a
idbentley <ian.bentley@gmail.com>
2014-09-04 16:59:21 -0400

    bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='MongoTest',
-      version='0.1.1',
+      version='0.1.2',
       author="idbentley",
       author_email='ian.bentley@gmail.com',
       url="https://github.com/idbentley/mongo_test",
